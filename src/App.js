0c186611a159a5ce466d5222d6e1cf3ff478a55c
Jaecheol <jcgod413@gmail.com>
2018-02-28 15:31:13 +0900

    Add hyperlinks in App

--- src/App.js
+++ src/App.js
@@ -15,12 +15,15 @@ class App extends Component {
         </p>
         <div className="App-container">
           <p>How to play:</p>
-          <p>1. Send 1 (exact) to address 0x84c6b8b792be03b006c5d87ebc3330f7ac28f7a6 (TESTNET Ropsten at the moment)</p>
+          <p>1. Send 1 (exact) to address " 0x3fcdcea917a1342648badf4cb976740c2ea5db9f " (TESTNET Ropsten at the moment)</p>
           <p>2. After 5 people have sent their bets, the contract chooses one address at random and pays 80 % of the total payout available to the winner</p>
-          <p>3. The rest goes to GiveDirectly</p>
+          <p>3. The rest goes to: 
+            <a href="https://www.givedirectly.org/">GiveDirectly</a>
+          </p>
           <p>4. The game restarts</p>
-          <p>5. sourcecode: ethereum lottery</p>
-
+          <p>5. sourcecode:
+          <a href="https://github.com/KISA-Bet">Github</a>
+          </p>
           <p></p>
           <p>The number of sign ups so far is 5</p>
         </div>
